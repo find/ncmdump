304f84d1eb823732ef8ee90b0c5a02ee129560e8
if <uint64_t@126.com>
2020-11-26 00:37:30 +0800

    cleanup xmake script

--- xmake.lua
+++ xmake.lua
@@ -1,30 +1,18 @@
-local taglibdirs = (function(t)
-  local r={}
-  for _,v in ipairs(t) do
-    local s=string.gsub(v, '\\', '/')
-    --print('--'..s..'--')
-    table.insert(r, s)
-  end
-  return r
-end)(os.dirs('taglib/taglib/**'))
---for i,v in ipairs(taglibdirs) do print(i,type(v),v) end
-
 add_defines('TAGLIB_STATIC')
+add_includedirs('taglib/taglib', unpack(os.dirs('taglib/taglib/**')))
+add_includedirs('$(buildir)')
+
 target('taglib')
   set_kind('static')
   before_build(function(target)
     os.cp('taglib/taglib/taglib_config.h.cmake', '$(buildir)/taglib_config.h')
   end)
   add_files('taglib/taglib/**.cpp')
-  add_includedirs('taglib/taglib', unpack(taglibdirs))
   add_includedirs('taglib/3rdparty')
-  add_includedirs('$(buildir)')
 
 target('ncmdump')
   set_kind('binary')
   set_default(true)
   add_files('*.cpp')
-  add_includedirs('taglib/taglib', unpack(taglibdirs))
-  add_includedirs('$(buildir)')
   add_deps('taglib')
 
